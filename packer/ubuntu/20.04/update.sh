b9b4b3f90c660210108d5d5c711ec9cc6ce9cf86
claughinghouse <claughinghouse@users.noreply.github.com>
2022-03-20 13:44:03 -0400

    feat: more cleanup

--- packer/ubuntu/20.04/update.sh
+++
@@ -1,8 +0,0 @@
-# install packages and upgrade
-echo "==> Updating list of repositories"
-apt-get -y update
-apt-get -y dist-upgrade
-
-# Clean up the apt cache
-apt-get -y autoremove --purge
-apt-get -y clean
